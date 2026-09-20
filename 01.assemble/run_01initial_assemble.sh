394b47b4944fbb265c9d1fae80587b2a4326a513
JiaYi Liang <168809032+linyuiz@users.noreply.github.com>
2026-09-20 13:51:46 +0800

    Update run_01initial_assemble.sh

--- 01.assemble/run_01initial_assemble.sh
+++ 01.assemble/run_01initial_assemble.sh
@@ -7,7 +7,6 @@ hifiasm $D_N_para $telo_txt --ont -o ${methods} -r $correct_round -t $threads -f
 🚀zgtools/overdose
 overdose hifiasm HIFI_ONT_HIC 60 2 3 3 2 ccs.fa pass.fq.gz hic_raw_R1.fq.gz hic_raw_R2.fq.gz $spec_type
 overdose hifiasm ONT_HIC 60 2 3 3 2 pass.fq.gz hic_raw_R1.fq.gz hic_raw_R2.fq.gz $spec_type
-
 ##✍️results like this:
 Genome                       Seq_Num  Total_Length   N50          GC(%)
 HIFI_ONT_HIC.primary.ctg.fa  121      *,***,150,468  102,418,633  43.96
@@ -20,7 +19,6 @@ export XDG_CACHE_HOME="$PWD/.cache"
 verkko --mbg /opt/conda/envs/verkko/bin//MBG --bwa /opt/conda/envs/verkko/bin/bwa-mem2 -d workdir --no-correction --hifi ccs.fa --nano ass.fq.gz --hic1 hic_raw_R1.fq.gz --hic2 hic_raw_R2.fq.gz --threads 60 --local --local-memory 700 --local-cpus 60
 🚀zgtools/overdose
 overdose Verkko HIFI_ONT_HIC 60 ccs.fa pass.fq.gz hic_raw_R1.fq.gz hic_raw_R2.fq.gz $spec_type
-
 ##✍️results like this:
 Genome                 Seq_Num  Total_Length   N50          GC(%)  Gap_Length
 verkko.diploid.ctg.fa  358      *,***,432,631  102,414,791  43.42  0
@@ -30,7 +28,6 @@ verkko.hap2.ctg.fa     163      *,***,366,537  101,995,084  43.32  0
 ##⭐️other assemble
 1️⃣shasta assemble(https://github.com/paoloshasta/shasta/)
 shasta --config Nanopore-r10.4.1_e8.2-400bps_sup-Raw-Jan2025 --input $input_tgs --threads 60 assemblyDirectory output_of_shasta
-
 ##✍️results like this:
 Genome             Seq_Num  Total_Length  N50        GC(%)
 Shasta_ONT.ctg.fa  47       52,127,603    1,515,432  47.39
