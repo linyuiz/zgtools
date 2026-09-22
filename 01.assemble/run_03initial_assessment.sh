d51060cb21ed5bf364c63fac341ca3e63fc36a6a
JiaYi Liang <168809032+linyuiz@users.noreply.github.com>
2026-09-22 10:39:35 +0800

    Update run_03initial_assessment.sh

--- 01.assemble/run_03initial_assessment.sh
+++ 01.assemble/run_03initial_assessment.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-##⭐️QV(https://github.com/marbl/merqury)
+⭐️QV(https://github.com/marbl/merqury)
 best_k.sh $genome_size 1>kmer.size 2>&1
 meryl k=${kmer_size} count meryl_R*.fastq.gz output db.meryl memory=750 threads=$threads
 merqury.sh db.meryl genome.fa genome
 🚀zgtools/overdose
 overdose qv HIFI_ONT_HIC.diploid.fa db.meryl
-##✍️results like this:
+✍️results like this:
 ID                    Length         N50          QV     Num  GC(%)   Completeness
 HIFI_ONT_HIC.diploid  *,***,170,225  102,414,868  71.60  139  43.36   99.9051
 
-##⭐️BUSCO(https://github.com/RoyNexus/busco)
+⭐️BUSCO(https://github.com/RoyNexus/busco)
 busco -o result -l /database/update/BUSCO/Latest/your_busco_odb10_or_12 -m geno -c 60 -i used.genome.fa --offline --long -f --miniprot
 🚀zgtools/overdose
 overdose BUSCO HIFI_ONT_HIC.diploid.fa geno miniprot your_busco_odb10_or_12 /database/update/BUSCO/Latest 60 simple
-##✍️results like this:
+✍️results like this:
 Item                                 Number  Percent(%)
 Complete BUSCOs (C)                  1328*   99.*
 Complete and single-copy BUSCOs (S)  37*     2.*
@@ -27,14 +27,14 @@ Hifiasm(HIFI+ONT+HIC)  C:99.*%[S:2.8%,D:96.*%],F:0.1%,M:0.3%
 Verkko(HIFI+ONT+HIC)   C:99.*%[S:2.8%,D:96.*%],F:0.1%,M:0.3%
 Hifiasm(ONT+HIC)       C:99.*%[S:3.0%,D:96.*%],F:0.1%,M:0.3%
 
-##⭐️N50plot(https://github.com/linyuiz/zgtools/)
+⭐️N50plot(https://github.com/linyuiz/zgtools/)
 🚀zgtools/overdose
 overdose n50plot HIFI_ONT_HIC.diploid.fa 60 Hifiasm_HIFI+ONT+HIC C:99.*%[S:2.8%,D:96.*%]QV:71.60
 
-##⭐️Telomere Count(https://github.com/linyuiz/zgtools/)
+⭐️Telomere Count(https://github.com/linyuiz/zgtools/)
 🚀zgtools/overdose
 overdose telomere HIFI_ONT_HIC.diploid.fa $spec_type
-##✍️results like this:
+✍️results like this:
 ID                     Telomere(≥10)  Telomere(≥100)  Telomere(≥500)
 Hifiasm(HIFI+ONT+HIC)  115            85              35
 Verkko(HIFI+ONT+HIC)   112            81              32
